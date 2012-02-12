47de2094f8f3867a10b2ef5bf1a48664124cc48f
Aapo Vienamo <aapo.vienamo@gmail.com>
2012-02-12 12:23:11 +0200

    Midi set channel and event functions made.

--- code/main.c
+++ code/main.c
@@ -1,40 +1,58 @@
-/* copyright Aapo Vienamo 2012 */
-/* Analong drum module midi interface firmware*/
-/* Made for atmega168 similar ones like atmega88, 8, 328 etc. would probably work also.*/
-/* System diagram: */
-/*
-           +-----------------+ 
-+------+   |                 |   +---------+
-|      |   |                 |	 |         |--> lines out 
-| MIDI |-->| usart       spi |-->|  74595  |
-| opto |   |                 |   |shift reg|--\ serial trough for daisy chaining
-+------+   |                 |   +---------+  |
-           |                 | /--------------/
-           |                 | | +---------+
-           |       AVR       | | |         |--> lines out
-           |    atmega168    | \>|  74595  |
-+--------+ |                 |   |         |
-|  DIP-  | |                 |   +---------+ amplifying from 0-5V to 0-15V
-|switches|>|                 |   +------------+   +---------+   +--------------+
-|chn sel | |       timer pwm |-->|rc lp filter|-->|  tl072  |-->|              |
-+--------+ |                 |   +------------+   |  opamp  |   |      4067    |
-           |                 |                    +---------+   | analog demux |
-           |                 |                                  |              |
-           |              io |--------------------------------->| pin adr      |--> lines out
- 					 +-----------------+                                  +--------------+
- */
-/* 	Design operational description */
-/* Midi messages are read in the interrupt where all but note on */
-/* events on correct channel (channel is set with DIP-switches on */
-/* the back of the device.) are ignored. When a valid midi event */
-/* arrives: */
-/*	1 	velocity of the note on is set to dac */
-/* 	2. 	analog demux updates that voltage to appropriate pin */
-/*	   	pin is determined by note to which the drum voice is set */
-/*  3. 	an spi package is crafted and sent to shift registers to */
-/*		  trigger the drum voice.  */
-/* Outputs of shift registers aro connected to trigger pins of the drum modules. */
-/* Analog demux lines are connected to accent pins. */
+/* 
+
+	Copyright (C) 2011 Aapo Vienamo
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+	Analong drum module midi interface firmware
+  Made for atmega168 similar ones like atmega88, 8, 328 etc. would probably work also.
+ 	System diagram: 
+
+	           +-----------------+ 
+	+------+   |                 |   +---------+
+	|      |   |                 |	 |         |--> lines out 
+	| MIDI |-->| usart       spi |-->|  74595  |
+	| opto |   |                 |   |shift reg|--\ serial trough for daisy chaining
+	+------+   |                 |   +---------+  |
+	           |                 | /--------------/
+	           |                 | | +---------+
+	           |       AVR       | | |         |--> lines out
+	           |    atmega168    | \>|  74595  |
+	+--------+ |                 |   |         |
+	|  DIP-  | |                 |   +---------+ amplifying from 0-5V to 0-15V
+	|switches|>|                 |   +------------+   +---------+   +--------------+
+	|chn sel | |       timer pwm |-->|rc lp filter|-->|  tl072  |-->|              |
+	+--------+ |                 |   +------------+   |  opamp  |   |      4067    |
+	           |                 |                    +---------+   | analog demux |
+	           |                 |                                  |              |
+  	         |              io |--------------------------------->| pin adr      |--> lines out
+	 					 +-----------------+                                  +--------------+
+
+	Design operational description
+	Midi messages are read in the interrupt where all but note on 
+	events on correct channel (channel is set with DIP-switches on 
+	the back of the device.) are ignored. When a valid midi event
+	arrives:
+ 	1 	velocity of the note on is set to dac
+	2. 	analog demux updates that voltage to appropriate pin
+	   	pin is determined by note to which the drum voice is set
+  3. 	an spi package is crafted and sent to shift registers to
+		  trigger the drum voice.
+ 	Outputs of shift registers aro connected to trigger pins of the drum modules.
+ 	Analog demux lines are connected to accent pins. 
+*/
 
 
 #include <avr/io.h>
@@ -48,53 +66,69 @@
 #define SS PB2
 #define MOSI PB3
 #define SCK PB5
+#define DAC_VAL OCR0A
 
 #define BAUD 31250
-#define BAUD_PRESCALE (((F_CPU / (31250 * 8UL))) - 1)
+#define BAUD_PRESCALE (((F_CPU / (31250 * 16UL))) - 1)
 
-void spi_master_init(void);
-void spi_transmit_byte(uint8_t byte);
-void spi_trigger_latch(void);
-void spi_transmit_word(uint16_t word);
+static inline void spi_master_init(void);
+static inline void spi_transmit_byte(uint8_t byte);
+static inline void spi_trigger_latch(void);
+static inline void spi_transmit_word(uint16_t word);
 
-void pwm_dac_init(void);
+static inline void pwm_dac_init(void);
 
-void usart_midi_init(uint16_t ubrr);
+static inline void usart_midi_init(uint16_t ubrr);
+/* Channels are counted from 0 not 1 */
+static inline void midi_set_listen_channel(uint8_t* data, uint8_t chnl);
+static inline void midi_set_listen_event(uint8_t* data, uint8_t event);
 
 typedef enum midi_event_stage_t{
-	event_type,
+	event_match,
 	note,
 	velocity
 } midi_event_stage_t;
 
+typedef struct midi_event_data_t{
+	uint8_t event_type, 
+					note, 
+					velocity;
+} midi_event_data_t;
+
 /*Midi channels range from 1 to 16 but with 4 bits you can numbers expres from 0 to 15.*/
-volatile uint8_t midi_listen_channel;
+volatile uint8_t midi_listen_event; /* Status event to for trigger. Contains note on and channel. */
 volatile uint8_t midi_trigger_event;
 volatile uint32_t midi_frame_data;
+volatile uint8_t note_offset=50;
 volatile midi_event_stage_t midi_event_stage;
+volatile midi_event_data_t midi_event_data;
 
 void main(void){
 	spi_master_init();
 	pwm_dac_init();
-	usart_midi_init(15);
+	usart_midi_init(BAUD_PRESCALE);
+	
+	//midi_listen_event = 0b10010000; /*note on on channel 1 */
+	midi_event_stage = event_match;
+	midi_set_listen_event(&midi_listen_event, 0b1001);
+	midi_set_listen_channel(&midi_listen_event, 8);
 
 	sei();
 
 	while(2){	
-		/*spi_transmit_word(i);*/
-		/*OCR0A = (uint8_t)i ^ (~i<<3) ^ (i<<7) ;*/
-		/*OCR0A = (uint8_t)i;*/
-		/*_delay_ms(30);*/
-		/*i++;*/
-		/*for(i = 0; i < 16; i++){*/
-		/*spi_transmit_word(1<<i);*/
 		_delay_ms(10);
-		/*}*/
-		spi_transmit_word(0);
+		spi_transmit_word(0x0); /* To clean it. This is just for debugging! Wont be in final releas. */
 	}
 }
+static inline void midi_set_listen_event(uint8_t* data, uint8_t event){
+	*data = (*data & 0b00001111) + (event << 4);
+}
 
-void usart_midi_init(uint16_t ubrr){
+static inline void midi_set_listen_channel(uint8_t* data, uint8_t chnl){
+	*data = (*data & 0b11110000) + chnl;
+}
+
+static inline void usart_midi_init(uint16_t ubrr){
 	UBRR0H = (uint8_t) (ubrr>>8);
 	UBRR0L = (uint8_t) ubrr;
 	
@@ -103,7 +137,7 @@ void usart_midi_init(uint16_t ubrr){
 
 }
 
-void usart_transmit(uint8_t data){
+static inline void usart_transmit(uint8_t data){
 	while(!(UCSR0A & (1<<UDRE0)))
 		;
 	UDR0 = data;
@@ -111,46 +145,56 @@ void usart_transmit(uint8_t data){
 
 ISR(USART_RX_vect){
 	uint8_t data = UDR0;
-	switch(midi_event_stage){
-		case event_type: 
-			if((data&0x0f) == midi_listen_channel){ 
+	switch(midi_event_stage){ /* Or how about using that struct as a list :p */
+		case event_match: 
+			if(data == midi_listen_event){
 				spi_transmit_word(0xffff);
+				midi_event_data.event_type = data;
 				midi_event_stage++;
 			}
 			break;
+		case note:
+			midi_event_data.note = data;
+			midi_event_stage++;
+			break;
+		case velocity:
+			midi_event_data.velocity = data;
+			DAC_VAL = (data<<1);			
+			midi_event_stage = event_match;
 		default:
 			break;
 	}
 
 }
 
-void pwm_dac_init(void){
+static inline void pwm_dac_init(void){
 	DDRD |= (1<<OC0A);
 	TCCR0A = (1<<COM0A1)|(1<<WGM01)|(1<<WGM00); /* clear on compare match, fast pwm*/
 	TCCR0B = (1<<CS00); /*no clock prescaling f=fcpu*/
 	/*OCR0A = 0xfa; */
 }
 
-void spi_master_init(void){
+static inline void spi_master_init(void){
 	/*SS, MOSI and SCK pins set as output*/
 	DDRB |= (1<<SS)|(1<<MOSI)|(1<<SCK);
 	/*spi enabled, master mode, no clock bits set so it will be fosc/4*/
 	SPCR = (1<<SPE)|(1<<MSTR);
 }
 
-void spi_trigger_latch(void){
+static inline void spi_trigger_latch(void){
 	/*triggering output latch on 74595*/
 	PORTB |= (1<<SS);
 	PORTB &= ~(1<<SS);
 }
 
-void spi_transmit_byte(uint8_t byte){
+static inline void spi_transmit_byte(uint8_t byte){
 	spi_send(byte);	
+	//spi_send(0);
 	spi_trigger_latch();
 	spi_trigger_latch();
 }
 
-void spi_transmit_word(uint16_t word){	
+static inline void spi_transmit_word(uint16_t word){	
 	spi_send((uint8_t)(word>>8));
 	spi_send((uint8_t)word);
 	spi_trigger_latch();
